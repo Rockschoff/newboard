c2ed8f992913997d6df442d3f37b7555dac5f44b
Rishi Dubey <50651298+Rockschoff@users.noreply.github.com>
2024-09-10 17:30:29 -0700

    small update

---
+++ CAPA/CAPA.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def render_CAPA():
+    with st.exception("CAPA"):
+        st.subheader("CAPA")
--- streamlit.py
+++ streamlit.py
@@ -2,6 +2,7 @@ import streamlit as st
 from process_control.process_control import render_process_control
 from prereq_and_verif.prereq_and_verif import render_preq_and_verif
 from supplier_management.supplier_management import render_supplier_management
+from CAPA.CAPA import render_CAPA
 from map import render_map
 
 from sidebar import render_sidebar
@@ -15,5 +16,6 @@ render_map()
 render_preq_and_verif()
 render_process_control()
 render_supplier_management()
+render_CAPA()
 
 
