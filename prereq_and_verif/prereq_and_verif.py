3f19e617c827ff1b1a5a5a827abaaf979c241262
Rishi Dubey <50651298+Rockschoff@users.noreply.github.com>
2024-09-10 17:44:14 -0700

    small update

--- prereq_and_verif/prereq_and_verif.py
+++ prereq_and_verif/prereq_and_verif.py
@@ -67,7 +67,7 @@ def render_preventive_maintanence():
 
 def render_sanitation():
     st.subheader("Sanitation")
-    st.error("Critical Error Demo")
+    st.error("3 ATP tests failed, equipment resanitized")
     st.warning("Not So Critical Error Demo")
     images1 = ["./prereq_and_verif/static/sanitation/visual_inspection.png" , "./prereq_and_verif/static/sanitation/atp_test.png"]
     images2 = ["./prereq_and_verif/static/sanitation/EMP_grouped.png"]
