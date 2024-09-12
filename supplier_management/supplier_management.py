2f0edd45c90a45305618873bc432faf6d8160e6f
Rishi Dubey <50651298+Rockschoff@users.noreply.github.com>
2024-09-12 15:34:25 -0700

    add horizon scanning

--- supplier_management/supplier_management.py
+++ supplier_management/supplier_management.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def render_supplier_management():
     with st.expander("Supplier Management"):
-        kpi_names = ["Supplier Performance" , "Material Performance" , "Exceptions"]
+        kpi_names = ["Supplier Performance" , "Material Performance" , "Exceptions", "Horizon Scanning"]
         tabs = st.tabs(kpi_names)
         for i , tab in enumerate(tabs):
             with tab:
